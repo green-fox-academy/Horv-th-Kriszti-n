40206dbce8cbea9a3f4c387d73d17b446edb622c
Horváth Krisztián <krisztianhor@gmail.com>
2018-11-07 11:07:02 +0100

    First 2 exercises are done

---
+++ week-2/day-3/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //        int[] xPoints = {10, 110, 110, 10};
+        //        int[] yPoints = {10, 10, 110, 110};
+        //        graphics.drawPolygon(xPoints, yPoints, 4); // <-- nPoints mean how many edge points do our polygon have
+    }
+}
