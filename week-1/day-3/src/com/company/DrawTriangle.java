28da214a5e6d6b67e28b0c3a7d22d6fee696e8a7
Horváth Krisztián <krisztianhor@gmail.com>
2018-11-11 20:40:53 +0100

    Add DrawTriangle

---
+++ week-1/day-3/src/com/company/DrawTriangle.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class DrawTriangle {
+    public static void main(String[] args) {
+        // Write a program that reads a number from the standard input, then draws a
+        // triangle like this:
+        //
+        // *
+        // **
+        // ***
+        // ****
+        //
+        // The triangle should have as many lines as the number was
+        System.out.print("Please give me the size: ");
+        Scanner input = new Scanner(System.in);
+        int userInput = input.nextInt();
+        getTriangle(userInput);
+    }
+    public static void getTriangle(int inputNumber){
+        for (int i = 0; i < inputNumber; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
