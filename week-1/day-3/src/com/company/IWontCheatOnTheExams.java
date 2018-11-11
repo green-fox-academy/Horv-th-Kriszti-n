c670a7d3d16b817c41a437d823da780020a80ee3
Horváth Krisztián <krisztianhor@gmail.com>
2018-11-11 15:36:24 +0100

    Add IWontCheatOnThe Exam!

---
+++ week-1/day-3/src/com/company/IWontCheatOnTheExams.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class IWontCheatOnTheExams {
+    // Create a program that writes this line 100 times:
+    // "I won't cheat on the exam!"
+    public static void main(String[] args) {
+        noCheat();
+    }
+    public static void noCheat(){
+        for (int i = 0; i < 100; i++) {
+            System.out.println("I won't cheat on the exam!");
+        }
+
+    }
+}
