85d34316ec18c6a7917f4df12960c0da11c90fe5
Horváth Krisztián <krisztianhor@gmail.com>
2018-11-13 15:04:24 +0100

    Added Animal

---
+++ week-3/day-2/src/animal/Animal.java
@@ -0,0 +1,21 @@
+package animal;
+
+public class Animal {
+    int hunger;
+    int thirst;
+
+    Animal(int hunger, int thirst){
+        this.hunger = hunger;
+        this.thirst = thirst;
+    }
+    public void eat(){
+        System.out.println("Hunger: " + (hunger - 1));
+    }
+    public void drink(){
+        System.out.println("Thirst: " + (thirst -1));
+    }
+    public void play(){
+        System.out.print("Hunger: " + (hunger + 1));
+        System.out.print(" Thirst: " + (thirst + 1));
+    }
+}
---
+++ week-3/day-2/src/animal/Main.java
@@ -0,0 +1,11 @@
+package animal;
+
+public class Main {
+    public static void main(String[] args) {
+    Animal fox = new Animal(50, 50);
+
+    fox.eat();
+    fox.drink();
+    fox.play();
+    }
+}
