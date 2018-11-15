58bb2227bba225f89eda18534ea76cd6938869c5
Horváth Krisztián <krisztianhor@gmail.com>
2018-11-15 16:05:27 +0100

    Added Bunnies

---
+++ week-3/day-4/src/bunnies/Bunnies.java
@@ -0,0 +1,15 @@
+package bunnies;
+
+public class Bunnies {
+  public static void main(String[] args) {
+    // We have a number of bunnies and each bunny has two big floppy ears.
+    // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+    System.out.println(EarOfBunnies(5, 2));
+  }
+  public static int EarOfBunnies(int bunny, int ear){
+    if (bunny == 0){
+    return 0;
+    }
+    return ear + EarOfBunnies(bunny-1, ear);
+  }
+}
