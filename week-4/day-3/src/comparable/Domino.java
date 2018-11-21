316800301fc003934ece8a7e5ffe2e2ca19447c6
Horváth Krisztián <krisztianhor@gmail.com>
2018-11-21 16:35:25 +0100

    Added Comparable

---
+++ week-4/day-3/src/comparable/Domino.java
@@ -0,0 +1,31 @@
+package comparable;
+
+import java.util.Arrays;
+
+public class Domino implements Comparable<Domino> {
+  private final int left;
+  private final int right;
+
+  public Domino(int left, int right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  public int getLeftSide() {
+    return left;
+  }
+
+  public int getRightSide() {
+    return right;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + left + ", " + right + "]";
+  }
+
+  public int compareTo(Domino compareNumber) {
+    int compareFirstValue = compareNumber.getLeftSide();
+    return this.left - compareFirstValue;
+  }
+}
